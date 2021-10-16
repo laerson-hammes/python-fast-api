a9dadcea8336d35c493802ca04ecc333724b334c
laerson-hammes <laersonha@gmail.com>
2021-10-16 17:19:17 -0300

    Listagem de Itens

--- api_requests/api.py
+++ api_requests/api.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, Request
+from api_requests.schema import Item
+from api_requests.exceptions import PedidoNaoEncontradoError, FalhaDeComunicacaoError
+from uuid import UUID
+from fastapi.responses import JSONResponse
+from http import HTTPStatus
+from api_requests.magalu_api import recuperar_itens_por_pedido
 
 
 app = FastAPI()
 
 
+@app.exception_handler(FalhaDeComunicacaoError)
+def tratar_erro_falha_de_comunicacao(request: Request, exc: FalhaDeComunicacaoError):
+    return JSONResponse(status_code=HTTPStatus.BAD_GATEWAY, content={"message": "Falha de comunicação com o servidor remoto"})
+
+
+@app.exception_handler(PedidoNaoEncontradoError)
+def tratar_erro_pedido_nao_encontrado(request: Request, exc: PedidoNaoEncontradoError):
+    return JSONResponse(status_code=HTTPStatus.NOT_FOUND, content={"message": "Pedido não encontrado"})
+
+
 @app.get("/healthcheck")
 async def healthcheck():
     return {"status": "ok"}
+
+
+@app.get("/orders/{identificacao_do_pedido}/items")
+def listar_itens(itens: list[Item] = Depends(recuperar_itens_por_pedido)):
+    return itens
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,27 +1,77 @@
 from http import HTTPStatus
 
 import pytest
-from api_requests.api import app
+from api_requests.api import app, recuperar_itens_por_pedido
 from fastapi.testclient import TestClient
+from uuid import UUID
+from api_requests.schema import Item
+from api_requests.exceptions import PedidoNaoEncontradoError, FalhaDeComunicacaoError
 
 
 @pytest.fixture
 def client():
-   return TestClient(app)
+    return TestClient(app)
+
  
+@pytest.fixture
+def sobreescreve_recuperar_itens_por_pedido():
+   def _sobreescreve_recuperar_itens_por_pedido(itens_ou_erro):
+      def duble(identificacao_do_pedido: UUID) -> list[Item]:
+         if isinstance(itens_ou_erro, Exception):
+               raise itens_ou_erro
+         return itens_ou_erro
+      app.dependency_overrides[recuperar_itens_por_pedido] = duble
+   yield _sobreescreve_recuperar_itens_por_pedido
+   app.dependency_overrides.clear()
 
-def test_healthcheck(client):
-   response = client.get("/healthcheck")
-   assert response.status_code == HTTPStatus.OK
+
+class TestHealthCheck:
+   def test_devo_ter_como_retorno_codigo_de_status_200(self, client):
+      resposta = client.get("/healthcheck")
+      assert resposta.status_code == HTTPStatus.OK
+
+
+   def test_formato_de_retorno_deve_ser_json(self, client):
+      resposta = client.get("/healthcheck")
+      assert resposta.headers["Content-Type"] == "application/json"
+
+
+   def test_deve_conter_informacoes(self, client):
+      resposta = client.get("/healthcheck")
+      assert resposta.json() == {
+         "status": "ok",
+      }
+
+
+class TestListarPedidos:
+   def test_quando_identificacao_do_pedido_invalido_um_erro_deve_ser_retornado(self, client):
+      resposta = client.get("/orders/valor-invalido/items")
+      assert resposta.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
+   def test_quando_pedido_nao_encontrado_um_erro_deve_ser_retornado(self, client, sobreescreve_recuperar_itens_por_pedido):
+      sobreescreve_recuperar_itens_por_pedido(PedidoNaoEncontradoError())
+      resposta = client.get("/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items")
+      assert resposta.status_code == HTTPStatus.NOT_FOUND
+
+
+   def test_quando_encontrar_pedido_codigo_ok_deve_ser_retornado(self, client, sobreescreve_recuperar_itens_por_pedido):
+      sobreescreve_recuperar_itens_por_pedido([])
+      resposta = client.get("/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items")
+      assert resposta.status_code == HTTPStatus.OK
+
+
+   def test_quando_encontrar_pedido_deve_retornar_itens(self, client, sobreescreve_recuperar_itens_por_pedido):
+      itens = [
+         Item(sku='1', description='Item 1', image_url='http://url.com/img1', reference='ref1', quantity=1),
+         Item(sku='2', description='Item 2', image_url='http://url.com/img2', reference='ref2', quantity=2),
+      ]
+      sobreescreve_recuperar_itens_por_pedido(itens)
+      resposta = client.get("/orders/7e290683-d67b-4f96-a940-44bef1f69d21/items")
+      assert resposta.json() == itens
+      
    
-
-def test_healthcheck_return_json(client):
-   response = client.get("/healthcheck")
-   assert response.headers["Content-Type"] == "application/json"
-
-
-def test_quando_verificar_integridade_deve_conter_informacoes(client):
-   response = client.get("/healthcheck")
-   assert response.json() == {
-      "status": "ok"
-   }
+   def test_quando_fonte_de_pedidos_falha_um_erro_deve_ser_retornado(self, client, sobreescreve_recuperar_itens_por_pedido):
+      sobreescreve_recuperar_itens_por_pedido(FalhaDeComunicacaoError())
+      resposta = client.get("/orders/ea78b59b-885d-4e7b-9cd0-d54acadb4933/items")
+      assert resposta.status_code == HTTPStatus.BAD_GATEWAY
